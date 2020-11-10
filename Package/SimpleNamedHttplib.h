208c86cb29ebcd747bb02d56c12865632a536da0
THAT BOY <zhu.thatboy@outlook.com>
2020-11-10 16:41:57 +0800

    Add C# script

---
+++ Package/SimpleNamedHttplib.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#ifdef _WINDLL
+#define _SIMPLE_NAMED_HTTPLIB_API_ extern "C" __declspec(dllexport)
+#elif defined UNIT_TEST
+#define _SIMPLE_NAMED_HTTPLIB_API_
+#else
+#define _SIMPLE_NAMED_HTTPLIB_API_ extern "C" __declspec(dllimport)
+#endif
+
+using String = const char*;
+using OutString = char*;
+
+typedef void OnServerCloseDelegate(bool);
+typedef void RequestHandle(String request, String method, OutString response, OutString type);
+
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall CreateClient(String client_name, String host, int port);
+_SIMPLE_NAMED_HTTPLIB_API_ String _stdcall ClientPost(String client_name, String path, String body, String content_type);
+_SIMPLE_NAMED_HTTPLIB_API_ String _stdcall ClientGet(String client_name, String path);
+_SIMPLE_NAMED_HTTPLIB_API_ int _stdcall ClientCount();
+_SIMPLE_NAMED_HTTPLIB_API_ bool _stdcall ClientContain(String client_name);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ClientRemove(String client_name);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ClientClear();
+_SIMPLE_NAMED_HTTPLIB_API_ bool _stdcall TestServer(String host, int port);
+
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall CreateServer(String server_name, String host, int port);
+_SIMPLE_NAMED_HTTPLIB_API_ bool _stdcall ServerListen(String server_name);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ServerStop(String server_name);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall SetServerCloseDelegate(String server_name, OnServerCloseDelegate on_close);
+_SIMPLE_NAMED_HTTPLIB_API_ int _stdcall ServerCount();
+_SIMPLE_NAMED_HTTPLIB_API_ bool _stdcall ServerContain(String server_name);
+_SIMPLE_NAMED_HTTPLIB_API_ bool _stdcall ServerRunning(String server_name);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ServerRemove(String server_name);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ServerClear();
+
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ServerPost(String server_name, String pattern, RequestHandle handle);
+_SIMPLE_NAMED_HTTPLIB_API_ void _stdcall ServerGet(String server_name, String pattern, RequestHandle handle);
+
+_SIMPLE_NAMED_HTTPLIB_API_ String _stdcall Version();
+_SIMPLE_NAMED_HTTPLIB_API_ String _stdcall LastCompileTime();
+#undef _SIMPLE_NAMED_HTTPLIB_API_
---
+++ Package/SimpleNamedHttplibExport.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SimpleNamedHttplibExport
+{
+    class SimpleNamedHttplib
+    {
+        public delegate void OnServerCloseDelegate(bool success);
+        public delegate void RequestHandle(string request, string method, StringBuilder response, StringBuilder type);
+
+        private const string DllPath = @"SimpleNamedHttplib.dll";
+
+        [DllImport(DllPath)]
+        static extern void _stdcall CreateClient(string client_name, string host, int port);
+        [DllImport(DllPath)]
+        static extern string _stdcall ClientPost(string client_name, string path, string body, string content_type);
+        [DllImport(DllPath)]
+        static extern string _stdcall ClientGet(string client_name, string path);
+        [DllImport(DllPath)]
+        static extern int _stdcall ClientCount();
+        [DllImport(DllPath)]
+        static extern bool _stdcall ClientContain(string client_name);
+        [DllImport(DllPath)]
+        static extern void _stdcall ClientRemove(string client_name);
+        [DllImport(DllPath)]
+        static extern void _stdcall ClientClear();
+        [DllImport(DllPath)]
+        static extern bool _stdcall TestServer(string host, int port);
+
+        [DllImport(DllPath)]
+        static extern void _stdcall CreateServer(string server_name, string host, int port);
+        [DllImport(DllPath)]
+        static extern bool _stdcall ServerListen(string server_name);
+        [DllImport(DllPath)]
+        static extern void _stdcall ServerStop(string server_name);
+        [DllImport(DllPath)]
+        static extern void _stdcall SetServerCloseDelegate(string server_name, OnServerCloseDelegate on_close);
+        [DllImport(DllPath)]
+        static extern int _stdcall ServerCount();
+        [DllImport(DllPath)]
+        static extern bool _stdcall ServerContain(string server_name);
+        [DllImport(DllPath)]
+        static extern bool _stdcall ServerRunning(string server_name);
+        [DllImport(DllPath)]
+        static extern void _stdcall ServerRemove(string server_name);
+        [DllImport(DllPath)]
+        static extern void _stdcall ServerClear();
+
+        [DllImport(DllPath)]
+        static extern void _stdcall ServerPost(string server_name, string pattern, RequestHandle handle);
+        [DllImport(DllPath)]
+        static extern void _stdcall ServerGet(string server_name, string pattern, RequestHandle handle);
+
+        [DllImport(DllPath)]
+        static extern string _stdcall Version();
+        [DllImport(DllPath)]
+        static extern string _stdcall LastCompileTime();
+    }
+}
